2efa3e2691cf00c5af2ec43f26d5d4dca58e8bc9
Tyler Hoffman <tylerhoffman@Tylers-MacBook-Pro-2.local>
2022-11-14 22:02:34 -0800

    Create/Clear game board ()

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-// Create Game Board
+/*
+    Create/Clear the Game Board
+*/
 
 const gameArea = document.querySelector('.game-container');
-let alt = 1;
-for (let i = 0; i < 64; i++) {
-    if (i % 8 === 0) alt++;
-    let boxClass = alt % 2 === 0 ? (i % 2 === 0 ? 'white' : 'black') : (i % 2 === 0 ? 'black' : 'white');
 
-    const newBox = document.createElement('div');
-    newBox.classList.add(boxClass);
-    newBox.id = i;
-    gameArea.appendChild(newBox);
+function clearBoard() {
+    let alt = 0;
+    for (let i = 0; i < 64; i++) {
+        if (i % 8 === 0) alt++;
+        let boxClass = alt % 2 === 0 ? (i % 2 === 0 ? 'white' : 'black') : (i % 2 === 0 ? 'black' : 'white');
+        const newBox = document.createElement('div');
+        newBox.classList.add(boxClass);
+        newBox.id = i;
+        gameArea.appendChild(newBox);
+        if (alt <= 3 && boxClass === 'white') {
+            checker = document.createElement('div');
+            checker.classList.add('white_checker');
+            newBox.appendChild(checker);
+        } else if (alt >= 6 && boxClass === 'white') {
+            checker = document.createElement('div');
+            checker.classList.add('black_checker');
+            newBox.appendChild(checker);
+        }
+    }
 }
+
+clearBoard();
